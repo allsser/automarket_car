3ed62ccc0fd7a31ffdee62d396b6e13cd17d8849
allsser <allsser@naver.com>
2019-10-16 21:29:10 +0900

    t1

--- src/application/VCSController.java
+++ src/application/VCSController.java
@@ -369,7 +369,7 @@ public class VCSController {
 		Conn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				String portName = "COM7";
+				String portName = "COM10";
 				connectPort(portName);
 				
 			}
